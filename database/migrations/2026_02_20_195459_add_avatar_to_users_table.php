b2f5c9368ef054bcc69a606ade2a7cccb634a2f5
Angeles Salas <angelessalasgonzalez28@gmail.com>
2026-02-20 19:53:37 -0600

    Migrate

---
+++ database/migrations/2026_02_20_195459_add_avatar_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Illuminate\Database\Migrations\Migrations\Migration;
+use Illuminate\Database\Illuminate\Database\Blueprint;
+use Illuminate\Database\Illuminate\Database\Schema;
+
+return new class extends Migration
+{
+
+
+    public function up(): void
+    {
+       Schema::table('users', function (Blueprint $table){
+       
+
+       $table->string('avatar')->nullable()->after('name');
+       $table->string('telefono')->nullable()->after('avatar');
+       $table->string('calle')->nullable()->after('telefono');
+    });
+    }
+
+
+
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+        $table->dropColumn('avatar');
+        });
+    }
+};
